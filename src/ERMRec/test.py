tjanez/PyMTL

commit 52797ae8d35b40d2545b2f3bb561d4d62d15a9e3

NEW Implemented visualization of ERM merging history via dendrograms:   - Augmented the MergedUser class to store the merging history information.   - Added the convert_merg_history_to_scipy_linkage() function that converts the given merging history to the same format as returned by scipy.cluster.hierarchy.linkage function.   - Added plot_dendrograms() function to the plotting module which plots multiple dendrograms on one figure.   - Added _draw_dendrogram() function to the plotting module which draws one particular linkage information as a dendrogram.   - Added a simple small example to the plotting module.   - Augmented the ERMLearner's return value, which now also returns a list of tuples as returned by the convert_merg_history_to_scipy_linkage function.   - Implemented a generic return value for all Learners, which is a dictionary holding an arbitrary number of data structures.   - Augmented UsersPool.test_users() function to handle a new results_path argument and plots a dendrogram if the results contain the dend_info object. FIX Fixed a bug in the UsersPool.visualize_results() function. It had the path_prefix argument, which wasn't used. Instead, it used the global results_path variable, which was incidentally the same as the path_prefix argument. Upon fixing this, path_prefix arguement was renamed to results_path.

src/ERMRec/test.py
@@ -30,7 +30,8 @@ from sklearn import cross_validation
 from ERMRec import stat
 from ERMRec.data import load_ratings_dataset
 from ERMRec.learning import prefiltering, learning
-from ERMRec.plotting import BarPlotDesc, LinePlotDesc, plot_multiple
+from ERMRec.plotting import BarPlotDesc, LinePlotDesc, plot_multiple, \
+    plot_dendrograms
 
 def configure_logging(name=None, level=logging.DEBUG,
                       console_level=logging.DEBUG,
@@ -478,7 +479,7 @@ class UsersPool:
                     100.*m_errors/n))
         return scores
     
-    def test_users(self, learners, base_learners, measures):
+    def test_users(self, learners, base_learners, measures, results_path):
         """Divide all users' data into folds and perform the tests on each fold.
         Test the performance of the given learning algorithms with the given
         base learning algorithms and compute the testing results using the
@@ -499,6 +500,9 @@ class UsersPool:
             name and learner is an Orange learner
         measures -- list of strings representing measure's names (currently,
             only CA and AUC are supported)
+        results_path -- string representing the path where to save any extra
+            information about the running of this test (currently, just ERM's
+            dendrograms)
         
         """
         # divide users' data into folds
@@ -514,12 +518,19 @@ class UsersPool:
             for bl in base_learners:
                 for l in learners:
                     start = time.clock()
-                    user_models = learners[l](self._users, base_learners[bl])
-                    fold_scores[i][bl][l] = self._test_users(user_models,
+                    R = learners[l](self._users, base_learners[bl])
+                    fold_scores[i][bl][l] = self._test_users(R["user_models"],
                                                              measures)
                     end = time.clock()
                     logger.debug("Finished fold: {}, base learner: {}, "
                         "learner: {} in {:.2f}s".format(i, bl, l, end-start))
+                    # plot a dendrogram showing merging history if the results
+                    # contain dendrogram info 
+                    if "dend_info" in R:
+                        plot_dendrograms(R["dend_info"], os.path.join(
+                            results_path, "dend-{}-fold{}.png".format(bl, i)),
+                            title="Merging history of ERM with base learner {}"
+                            " (fold {})".format(bl, i))
         # compute the average measure scores over all folds
         avg_scores = _compute_avg_scores(fold_scores)
         # get users' hashes
@@ -644,7 +655,7 @@ class UsersPool:
             ci95s.append(stat.ci95(scores))
         return avgs, stds, ci95s
         
-    def visualize_results(self, base_learners, learners, measures, path_prefix,
+    def visualize_results(self, base_learners, learners, measures, results_path,
                           colors, plot_type="line"):
         """Visualize the results of the given learning algorithms with the given
         base learning algorithms and the given scoring measures on the pool of
@@ -662,8 +673,8 @@ class UsersPool:
         base_learners -- list of strings representing the names of base learners
         learners -- list of strings representing the names of learners
         measures -- list of strings representing names of the scoring measures
-        path_prefix -- string representing the prefix of the path where to save
-            the generated plots
+        results_path -- string representing the path where to save the generated
+            plots
         colors -- dictionary mapping from learners' names to the colors that
             should represent them in the plots
             
@@ -710,14 +721,20 @@ class UsersPool:
                     else:
                         raise ValueError("Unsupported plot type: '{}'".\
                                          format(plot_type))
-            plot_multiple(plot_desc_sd, results_path+"/{}-avg-SD.pdf".\
-                    format(m), title="Avg. results for groups of users (error"
-                    " bars show std. dev.)", subplot_title_fmt="Learner: {}",
-                    xlabel="Number of ratings", ylabel=m)
-            plot_multiple(plot_desc_ci95, results_path+"/{}-avg-CI.pdf".\
-                    format(m), title="Avg. results for groups of users (error"
-                    " bars show 95% conf. intervals)", subplot_title_fmt=\
-                    "Learner: {}", xlabel="Number of ratings", ylabel=m)
+            plot_multiple(plot_desc_sd,
+                os.path.join(results_path, "/{}-avg-SD.pdf".format(m)),
+                title="Avg. results for groups of users (error bars show std. "
+                    "dev.)",
+                subplot_title_fmt="Learner: {}",
+                xlabel="Number of ratings",
+                ylabel=m)
+            plot_multiple(plot_desc_ci95,
+                os.path.join(results_path, "/{}-avg-CI.pdf".format(m)),
+                title="Avg. results for groups of users (error bars show 95% "
+                    "conf. intervals)",
+                subplot_title_fmt="Learner: {}",
+                xlabel="Number of ratings",
+                ylabel=m)
 
 if __name__ == "__main__":
     # a boolean indicating which pool of users to use
@@ -810,7 +827,7 @@ if __name__ == "__main__":
     
     # test all combinations of learners and base learners (compute the testing
     # results with the defined measures) and save the results
-    pool.test_users(learners, base_learners, measures)
+    pool.test_users(learners, base_learners, measures, results_path)
     pool.pickle_test_results(pickle_path_fmt)
     
     # find previously computed testing results and check if they were computed

src/ERMRec/learning/learning.py
@@ -39,8 +39,10 @@ class MergeAllLearner:
     def __call__(self, users, base_learner):
         """Run the merging algorithm for the given users. Learn a single model
         on the merger of all users' data using the given base learner.
-        Return a dictionary mapping from users' ids to the learned models (in
-        this case, all users' ids will map to the same model).
+        Return a dictionary of data structures computed within this learner.
+        It has the following keys:
+            user_models -- dictionary mapping from users' ids to the learned
+                models (in this case, all users' ids will map to the same model)
         
         Arguments:
         users -- dictionary mapping from users' ids to their User objects
@@ -61,7 +63,10 @@ class MergeAllLearner:
         user_models = dict()
         for user_id in users:
             user_models[user_id] = base_learner
-        return user_models
+        # create and fill the return dictionary
+        R = dict()
+        R["user_models"] = user_models
+        return R
 
 class NoMergingLearner:
     
@@ -73,7 +78,10 @@ class NoMergingLearner:
     def __call__(self, users, base_learner):
         """Run the merging algorithm for the given users. Learn a model using
         the given base learner for each user on its own data (no merging).
-        Return a dictionary mapping from users' ids to the learned models.
+        Return a dictionary of data structures computed within this learner.
+        It has the following keys:
+            user_models -- dictionary mapping from users' ids to the learned
+                models
         
         Arguments:
         users -- dictionary mapping from users' ids to their User objects
@@ -99,7 +107,10 @@ class NoMergingLearner:
                 model = clone(base_learner)
                 model.fit(*learn)
             user_models[user_id] = model
-        return user_models
+        # create and fill the return dictionary
+        R = dict()
+        R["user_models"] = user_models
+        return R
 
 import random, sys
 from collections import Iterable, OrderedDict
@@ -185,6 +196,59 @@ def flatten(l):
             flat_l.append(el)
     return flat_l
 
+def convert_merg_history_to_scipy_linkage(merg_history):
+        """Convert the given merging history to same format as returned by
+        the SciPy's scipy.cluster.hierarchy.linkage function.
+        Return a tuple (Z, labels), where:
+            Z -- numpy.ndarray of size (len(merg_history), 4) in the format
+                as specified in the scipy.cluster.hierarchy.linkage's docstring
+            labels -- list of labels representing ids corresponding to each
+                consecutive integer
+        
+        Arguments:
+        merg_history -- a list of lists, where each inner list contains ids of
+            single and merged users (the first in the form of strings and the
+            second in the form of tuples) 
+        
+        """
+        # dictionary mapping from user ids to consecutive integers
+        id_to_int = OrderedDict()
+        # current consecutive integer
+        cur_int = 0
+        # convert ids of single users to consecutive integers
+        for merg in merg_history:
+            for id in merg:
+                if not isinstance(id, tuple):
+                    id_to_int[id] = cur_int
+                    cur_int += 1
+        # total number of single users
+        n = len(id_to_int)
+        # create a list of labels (i.e. an implicit reverse mapping from
+        # consecutive integers to ids)
+        labels = [t[0] for t in sorted(id_to_int.iteritems(),
+                                       key=lambda t: t[1])]
+        # current 'height' and its increment value
+        inc = 1
+        cur_h = inc
+        # convert the merging history to scipy linkage format
+        Z = np.zeros((n - 1, 4))
+        for i, merg in enumerate(merg_history):
+            # number of users in the current merger
+            cur_n = 0
+            for j, id in enumerate(merg):
+                if id not in id_to_int:
+                    id_to_int[id] = cur_int
+                    cur_int += 1
+                    cur_n += Z[id_to_int[id] - n, 3]
+                else:
+                    cur_n += 1
+                Z[i, j] = id_to_int[id]
+            Z[i, 3] = cur_n
+            # store the current 'height' and increment its value
+            Z[i, 2] = cur_h
+            cur_h += inc
+        return Z, labels
+
 class MergedUser:
     
     """Contains data pertaining to a particular (merged) user and methods for
@@ -207,16 +271,25 @@ class MergedUser:
             # create a copy of both numpy.arrays
             X, y = u.get_learn_data()
             self._learn = X.copy(), y.copy()
-        else:
+        elif len(users) == 2:
             # id is a hierarchically structured tuple of tuples representing the
             # history of the merged user (e.g. id "(5, (38, 40))" represents
             # the merged user that initially contained the merger of users
             # 38 and 40, which was later merged with user 5)
-            self.id = tuple([u.id for u in users])
+            self.id = (users[0].id, users[1].id)
+            # combine merging history for users that have it already
+            self.merg_history = []
+            for u in users:
+                if hasattr(u, "merg_history"):
+                    self.merg_history.extend(u.merg_history)
+            self.merg_history.append([users[0].id, users[1].id])
             # merge learning data of all users
             Xs_ys = [u.get_learn_data() for u in users]
             Xs, ys = zip(*Xs_ys)
             self._learn = np.concatenate(Xs, axis=0), np.concatenate(ys, axis=0)
+        else:
+            raise ValueError("Trying to merge more than 2 users is not "
+                             "possible!")
     
     def __str__(self):
         """Return a "pretty" representation of the merged user by indicating
@@ -339,6 +412,12 @@ class ERMLearner:
         intelligent merging of users' data according to the ERM learning method.
         After the merging is complete, build a model for each remaining (merged)
         user and assign this model to each original user of this (merged) user.
+        Return a dictionary of data structures computed within this call to ERM.
+        It has the following keys:
+            user_models -- dictionary mapping from each original user id to its
+                model
+            dend_info -- list of tuples (one for each merged user) as returned
+                by the convert_merg_history_to_scipy_linkage function
         
         Arguments:
         users -- dictionary mapping from users' ids to their User objects
@@ -410,8 +489,10 @@ class ERMLearner:
                         C[cp.key] = cp
             update_progress(1.* len(C) / n_cand, invert=True)
         print
-        # build a model for each remaining (merged) user
+        # build a model for each remaining (merged) user and store the info
+        # for drawing a dendrogram showing the merging history
         user_models = dict()
+        dend_info = []
         for merg_u_obj in self._users.itervalues():
             # NOTE: When the number of unique class values is less than 2, we
             # cannot fit an ordinary model (e.g. logistic regression). Instead,
@@ -431,9 +512,18 @@ class ERMLearner:
                 model = clone(self._base_learner)
                 model.fit(X, y)
             # assign this model to each original user of this (merged) user
-            for user_id in merg_u_obj.get_original_ids():
+            original_ids = merg_u_obj.get_original_ids()
+            for user_id in original_ids:
                 user_models[user_id] = model
-        return user_models
+            # store the dendrogram info (if the user is truly a merged user)
+            if len(original_ids) > 1:
+                dend_info.append(convert_merg_history_to_scipy_linkage(
+                                    merg_u_obj.merg_history))
+        # create and fill the return dictionary
+        R = dict()
+        R["user_models"] = user_models
+        R["dend_info"] = dend_info
+        return R
     
     def _estimate_errors_significances(self, u_1, u_2):
         """Estimate the average prediction errors of different models on

src/ERMRec/plotting.py
@@ -3,7 +3,7 @@
 # Contains classes and methods for storing plot descriptions and drawing
 # different plots showing the results of the learning algorithms.
 #
-# Copyright (C) 2012 Tadej Janez
+# Copyright (C) 2012, 2013 Tadej Janez
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -173,8 +173,101 @@ def plot_multiple(plot_descs_mult, file_name, title="", subplot_title_fmt="{}",
     for i, (bl, plot_descs) in enumerate(plot_descs_mult.iteritems()):
         _draw_subplot(axarr[i], plot_descs, title=subplot_title_fmt.format(bl),
                   xlabel=xlabel, ylabel=ylabel)
-    # adjust figure parameters to make it look nicer
     fig.suptitle(title)
+    # adjust figure parameters to make it look nicer
     fig.subplots_adjust(top=0.93, bottom=0.05, left=0.10, right=0.95,
                         wspace=0.2, hspace=0.25)
     fig.savefig(file_name)
+
+from scipy.cluster.hierarchy import dendrogram
+
+def _draw_dendrogram(axes, Z, labels=None):
+    """Draw the given linkage information as a dendrogram on the given Axes
+    object. Change the drawing parameters so that the dendrogram will blend
+    nicely into the figure showing multiple dendrograms.
+    
+    Arguments:
+    axes -- matplotlib.axes.Axes object where to draw the plot
+    Z -- numpy.ndarray in the format as specified in the
+        scipy.cluster.hierarchy.linkage's docstring
+    
+    Keyword arguments:
+    labels --  list or tuple (optional) where i-th value is the text to put
+        under the i-th leaf node
+    
+    """
+    # set current axes instance
+    plt.sca(axes)
+    # draw the dendrogram
+    dendrogram(Z, labels=labels, orientation="left")
+    # remove x-axis labels
+    axes.set_xticks(())
+    # remove the black border around axes
+    for spine in axes.spines.itervalues():
+        spine.set_visible(False)
+    # decrease the font size of y tick labels
+    for ytl in axes.get_yticklabels():
+        ytl.set_fontsize("small")
+
+def plot_dendrograms(dend_info, file_name, title="", ylabel=""):
+    """Plot multiple dendrograms on one figure.
+    The method stacks multiple dendrograms vertically and by removing subplots'
+    x-axis labels and black borders around axes creates an impression that the
+    whole figure is one big dendrogram.
+    
+    Arguments:
+    dend_info -- list of tuples (Z, labels), where:
+        Z -- numpy.ndarray in the format as specified in the
+            scipy.cluster.hierarchy.linkage's docstring
+        labels -- list of labels representing ids corresponding to each
+            consecutive integer
+    file_name -- string representing the path where to save the drawn figure
+    
+    Keyword arguments:
+    title -- string representing the title of the whole plot
+    ylabel -- string representing the figure's y axis label
+    
+    """
+    nplots = len(dend_info)
+    nrows = nplots
+    ncols = 1
+    fig = plt.figure()
+    # create an empty object array to hold all axes; it's easiest to make it 1-d
+    # so we can just append subplots upon creation
+    axarr = np.empty(nplots, dtype=object)
+    # Note: off-by-one counting because add_subplot uses the MATLAB 1-based
+    # convention.
+    for i in range(1, nplots+1):
+        axarr[i-1] = fig.add_subplot(nrows, ncols, i)
+    # draw dendrograms to the subplots
+    for i, (Z, labels) in enumerate(dend_info):
+        _draw_dendrogram(axarr[i], Z, labels)
+    fig.suptitle(title)
+    # draw a common y-axis label
+    ax = fig.add_axes([0., 0., 1., 1.])
+    ax.set_axis_off()
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.text(.05, 0.5, ylabel, rotation="vertical",
+            horizontalalignment="center", verticalalignment="center")
+    # adjust figure parameters to make it look nicer
+    fig.subplots_adjust(top=0.90, bottom=0.03, left=0.15, right=0.97,
+                        wspace=0.1, hspace=0.05)
+    fig.savefig(file_name)
+
+if __name__ == "__main__":
+    # A simple example with two linkages transformed in a figure with two
+    # dendrograms
+    Z = np.array([[0., 1., 1., 2.],
+                  [2., 3., 2., 2.],
+                  [4., 5., 3., 4.]])
+    labels = ['00009', '00038', '00016', '00033']
+    Z1 = np.array([[0., 1., 1., 2.],
+                   [2., 4., 2., 3.],
+                   [3., 5., 3., 4.]])
+    labels1 = ['00009', '00038', '00016', '00033']
+    dend_info = [(Z, labels), (Z1, labels1)]
+    file_name = "/home/tadej/Workspace/ERMRec/results/temp/dend.png"
+    plot_dendrograms(dend_info, file_name, title="Here comes the title",
+                     ylabel="y-label")
+    

commit bb0f54445e14a72cf0d705e11faf5004cb340fff

NEW Added support for running multiple experiments (one for each random seed) to test module: - Moved the code in the 'if __name__ == "__main__"' part around. - Implemented a new function remove_logger() that removes logger's handlers to enable correct logging after adding the support for running multiple experiments. - Renamed configure_logging() function to create_logger() since this is now more appropriate. BUG Fixed a bug in the path names in the visualize_results() function introduced by the previous commit (52797ae).

src/ERMRec/test.py
@@ -33,10 +33,10 @@ from ERMRec.learning import prefiltering, learning
 from ERMRec.plotting import BarPlotDesc, LinePlotDesc, plot_multiple, \
     plot_dendrograms
 
-def configure_logging(name=None, level=logging.DEBUG,
-                      console_level=logging.DEBUG,
-                      file_name=None, file_level=logging.DEBUG):
-    """Configure logging for the test module of ERMRec.
+def create_logger(name=None, level=logging.DEBUG,
+                  console_level=logging.DEBUG,
+                  file_name=None, file_level=logging.DEBUG):
+    """Create and configure a logger for the test module of ERMRec.
     Return the created Logger instance.
     
     Keyword arguments:
@@ -67,6 +67,19 @@ def configure_logging(name=None, level=logging.DEBUG,
         fh.setFormatter(formatter)
         logger.addHandler(fh)
     return logger
+
+def remove_logger(logger):
+    """Remove all handlers of the given Logger object.
+    
+    NOTE: This is a some-what dirty hack since the logging module doesn't
+    support removal of logger objects.
+    
+    Arguments:
+    logger -- logging.Logger object
+    
+    """
+    for handler in list(logger.handlers):
+        logger.removeHandler(handler)
 
 def pickle_obj(obj, file_path):
     """Pickle the given object to the given file_path.
@@ -722,14 +735,14 @@ class UsersPool:
                         raise ValueError("Unsupported plot type: '{}'".\
                                          format(plot_type))
             plot_multiple(plot_desc_sd,
-                os.path.join(results_path, "/{}-avg-SD.pdf".format(m)),
+                os.path.join(results_path, "{}-avg-SD.pdf".format(m)),
                 title="Avg. results for groups of users (error bars show std. "
                     "dev.)",
                 subplot_title_fmt="Learner: {}",
                 xlabel="Number of ratings",
                 ylabel=m)
             plot_multiple(plot_desc_ci95,
-                os.path.join(results_path, "/{}-avg-CI.pdf".format(m)),
+                os.path.join(results_path, "{}-avg-CI.pdf".format(m)),
                 title="Avg. results for groups of users (error bars show 95% "
                     "conf. intervals)",
                 subplot_title_fmt="Learner: {}",
@@ -740,41 +753,17 @@ if __name__ == "__main__":
     # a boolean indicating which pool of users to use
     test = True
     
-    rnd_seed = 51
+    # random seeds for which to repeat the experiment
+    if test:
+        rnd_seeds = range(51, 54)
+    else:
+        rnd_seeds = range(51, 61)
     # the number of users to keep in the users pool
     if test:
-        keep = 30
+        keep = 10
     else:
         keep = 100
-    
-    # compute the location of other files/directories from the current file's
-    # location
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    path_prefix = os.path.abspath(os.path.join(cur_dir, "../../"))
-    if test:
-        users_data_path = os.path.join(path_prefix, "data/users-test2")
-        results_path = os.path.join(path_prefix, "results/users-test2-seed{}-"
-                                    "keep{}".format(rnd_seed, keep))
-        if not os.path.exists(results_path):
-            os.makedirs(results_path)
-    else:
-        users_data_path = os.path.join(path_prefix, "data/users-m10")
-        results_path = os.path.join(path_prefix, "results/users-m10-seed{}-"
-                                    "keep{}".format(rnd_seed, keep))
-        if not os.path.exists(results_path):
-            os.makedirs(results_path)
-    pickle_path_fmt = os.path.join(results_path, "bl-{}.pkl")
-    log_file = os.path.join(results_path, "run-{}.log".format(
-                            time.strftime("%Y%m%d_%H%M%S")))
-    
-    # configure logging
-    logger = configure_logging(name="ERMRec", console_level=logging.INFO,
-                               file_name=log_file)
-    
-    # create a pool of users
-    pool = UsersPool(users_data_path, rnd_seed)
-    pool.only_keep_k_users(keep)
-    # select base learners
+
     base_learners = OrderedDict()
     from sklearn.linear_model import LogisticRegression
 #    from sklearn.pipeline import Pipeline
@@ -814,7 +803,7 @@ if __name__ == "__main__":
 #    # Canvas using data in user02984.tab
 #    base_learners["svm_RBF"] = svm.SVMLearner(svm_type=svm.SVMLearner.C_SVC,
 #        kernel_type=svm.SVMLearner.RBF, C=100.0, gamma=0.01, cache_size=500)
-    
+
     measures = []
     measures.append("CA")
     measures.append("AUC")
@@ -824,30 +813,62 @@ if __name__ == "__main__":
     learners["MergeAll"] = learning.MergeAllLearner()
     no_filter = prefiltering.NoFilter()
     learners["ERM"] = learning.ERMLearner(folds=5, seed=33, prefilter=no_filter)
+
+    for rnd_seed in rnd_seeds:    
+        # compute the location of other files/directories from the current
+        # file's location
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+        path_prefix = os.path.abspath(os.path.join(cur_dir, "../../"))
+        if test:
+            users_data_path = os.path.join(path_prefix, "data/users-test2")
+            results_path = os.path.join(path_prefix, "results/users-test2-"
+                                        "seed{}-keep{}".format(rnd_seed, keep))
+            if not os.path.exists(results_path):
+                os.makedirs(results_path)
+        else:
+            users_data_path = os.path.join(path_prefix, "data/users-m10")
+            results_path = os.path.join(path_prefix, "results/users-m10-seed{}-"
+                                        "keep{}".format(rnd_seed, keep))
+            if not os.path.exists(results_path):
+                os.makedirs(results_path)
+        pickle_path_fmt = os.path.join(results_path, "bl-{}.pkl")
+        log_file = os.path.join(results_path, "run-{}.log".format(
+                                time.strftime("%Y%m%d_%H%M%S")))
     
-    # test all combinations of learners and base learners (compute the testing
-    # results with the defined measures) and save the results
-    pool.test_users(learners, base_learners, measures, results_path)
-    pool.pickle_test_results(pickle_path_fmt)
+        # create a logger
+        logger = create_logger(name="ERMRec", console_level=logging.INFO,
+                               file_name=log_file)
     
-    # find previously computed testing results and check if they were computed
-    # using the same data tables and cross-validation indices
-    pool.find_pickled_test_results(pickle_path_fmt)
-    if not pool.check_test_results_compatible():
-        raise ValueError("Test results for different base learners are not "
-                         "compatible.")
-    # divide users into bins according to the number of ratings they have
-    if test:
-        bin_edges = [10, 15, 20]
-    else:
-        bin_edges = range(10, 251, 10)
-#    pool.divide_users_to_bins(bin_edges)
-    pool.divide_users_to_equally_sized_bins(n_bins=5)
-    # select the base learners, learners and scoring measures for which to 
-    # visualize the testing results
-    bls = pool.get_base_learners()
-    ls = pool.get_learners()
-    ms = pool.get_measures()
-    pool.visualize_results(bls, ls, ms, results_path,
-        colors={"NoMerging": "blue", "MergeAll": "green", "ERM": "red"},
-        plot_type="line")
+        # create a pool of users
+        pool = UsersPool(users_data_path, rnd_seed)
+        pool.only_keep_k_users(keep)
+        
+        # test all combinations of learners and base learners (compute the
+        # testing results with the defined measures) and save the results
+        pool.test_users(learners, base_learners, measures, results_path)
+        pool.pickle_test_results(pickle_path_fmt)
+    
+        # find previously computed testing results and check if they were
+        # computed using the same data tables and cross-validation indices
+        pool.find_pickled_test_results(pickle_path_fmt)
+        if not pool.check_test_results_compatible():
+            raise ValueError("Test results for different base learners are not "
+                             "compatible.")
+#        # divide users into bins according to the number of ratings they have
+#        if test:
+#            bin_edges = [10, 15, 20]
+#        else:
+#            bin_edges = range(10, 251, 10)
+#        pool.divide_users_to_bins(bin_edges)
+        pool.divide_users_to_equally_sized_bins(n_bins=5)
+        # select the base learners, learners and scoring measures for which to 
+        # visualize the testing results
+        bls = pool.get_base_learners()
+        ls = pool.get_learners()
+        ms = pool.get_measures()
+        pool.visualize_results(bls, ls, ms, results_path,
+            colors={"NoMerging": "blue", "MergeAll": "green", "ERM": "red"},
+            plot_type="line")
+        
+        # remove logger
+        remove_logger(logger)