8b76831a0b513768b03e6a865e86b7f25944c621
Tadej Janež <tadej.janez@tadej.hicsalta.si>
2013-04-29 14:49:23 +0200

    NEW Added ability to load the MNIST digits data set.

--- src/PyMTL/data.py
+++ src/PyMTL/data.py
@@ -75,11 +75,66 @@ def load_usps_digits_data():
                            DESCR=descr,
                            ID=id))
     return tasks
+
+
+def load_mnist_digits_data():
+    """Load the MNIST digits data set. Convert it from the original multi-way
+    classification problem to a MTL problem. 
     
+    The data set's properties are:
+    - 2000 samples
+    - 87 features (original images were pre-processed with PCA and their
+        dimensionality was reduced to 87, which retains ~95% of the total
+        variance.
+    - 10 classes, one for each of the 10 digits
+    
+    Note
+    ----
+    The data set is the one provided by "Kang, Grauman, Sha - Learning with Whom
+    to Share in Multi-task Feature Learning - ICML 2011" on their web site.
+    
+    Returns
+    -------
+    tasks -- list
+        A list of Bunch objects that correspond to binary classification tasks
+        of one digit against all the others.
+    
+    """
+    matlab_file = os.path.join(path_prefix, "data/mnist_digits/"
+                               "split1_mnist_1000train.mat")
+    mat = loadmat(matlab_file)
+    # combine training, validation and testing arrays into one big array
+    X = np.concatenate((mat["digit_trainx"], mat["digit_validx"],
+                        mat["digit_testx"]), axis=0)
+    y = np.concatenate((mat["digit_trainy"], mat["digit_validy"],
+                        mat["digit_testy"]), axis=0)
+    # convert this multi-way classification problem to a MTL problem
+    # (as described in Kang et al., ICML 2011)
+    tasks = []
+    for c in np.unique(y):
+        descr = "MNIST digits data: {} vs. other classes".format(c)
+        id = "{} vs. other".format(c)
+        cur_y = np.array(y, copy=True)
+        cur_y[y == c] = 1
+        cur_y[y != c] = 0
+        tasks.append(Bunch(data=X,
+                           target=cur_y,
+                           target_names=["other", str(c)],
+                           DESCR=descr,
+                           ID=id))
+    return tasks
+
 
 if __name__ == "__main__":
     tasks = load_usps_digits_data()
     print "Loaded the USPS digits MTL problem with the {} tasks:".\
+        format(len(tasks))
+    for t in tasks:
+        print t.DESCR
+    print
+    
+    tasks = load_mnist_digits_data()
+    print "Loaded the MNIST digits MTL problem with the {} tasks:".\
         format(len(tasks))
     for t in tasks:
         print t.DESCR
